7d7803faabde537a4d5421e6becabb41e16a7f99
Awais <awaisprojects1@gmail.com>
2022-10-27 15:21:02 +0500

    First Commit

---
+++ src/pages/home.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './home.styles.css';
+import Directory from '../components/directory/directory.component';
+
+const Home=()=>(
+    <div className="home">
+       <Directory />
+    </div>
+);
+
+export default Home;
